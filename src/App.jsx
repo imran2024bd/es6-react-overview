3fe413f74fb8937bcea7c909ea76459bd8376062
imran2024bd <imranitbd34@gmail.com>
2024-03-20 12:31:47 +0600

    41-3 Create And Load Fake Data, Chaptgpt Json, Online Api Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
+import Watch from './components/watch/Watch'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [watches , setWatches] = useState([]);
+
+  // from memory data
+  // useEffect(()=> {
+  //   fetch('watches.json')
+  //   .then(res => res.json())
+  //   .then ( data => setWatches(data));
+  // },[])
+
+// raw data from git hub link
+  useEffect(()=> {
+    fetch('https://raw.githubusercontent.com/imran2024bd/watches-json-data/main/watches.json')
+    .then(res => res.json())
+    .then ( data => setWatches(data));
+  },[])
+
+
+
+
+  // const watches =[
+  //   {id: 1, name: 'Apple watch', price: 200},
+  //   {id: 2, name: 'Samsung watch', price: 200},
+  //   {id: 3, name: 'MI watch', price: 200},
+  // ]
+
+
+//   const watches = [
+//     {
+//         id: 1,
+//         name: "Apple Watch Series 7",
+//         price: 399
+//     },
+//     {
+//         id: 2,
+//         name: "Galaxy Watch 4",
+//         price: 249
+//     },
+//     {
+//         id: 3,
+//         name: "Fitbit Sense",
+//         price: 299
+//     },
+//     {
+//         id: 4,
+//         name: "Garmin Venu 2",
+//         price: 399
+//     },
+//     {
+//         id: 5,
+//         name: "Huawei Watch GT 3",
+//         price: 199
+//     }
+// ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        watches.map(watch => <Watch key ={watch.id} watch ={watch}></Watch>)
+      }
     </>
   )
 }
 
 export default App
+
+
+// free open API link 
+// https://github.com/public-apis/public-apis
+// free mock api 
+// https://beeceptor.com/
+// swagger api
+// https://swagger.io/
--- src/components/watch/Watch.jsx
+++ src/components/watch/Watch.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Watch = () => {
+const Watch = ({watch}) => {
+    const {name , price} = watch;
     return (
         <div>
-            
+            <h2>watch : {name} </h2>
+            <p>price: {price}</p>
         </div>
     );
 };
 
 export default Watch;
+
+/*
+1. client side
+2. Api From Github
+3. Swagger
+4. Actual data
+*/
